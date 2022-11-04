e43e0cf792a4cb61c010f5f29562be494c17a8f7
Heidi Sánchez <heidisanchez103.9@gmail.com>
2022-11-04 16:03:59 -0500

    mi primer commit

---
+++ cypress/fixtures/examples/invalidLogin.js
@@ -0,0 +1,16 @@
+
+describe('Validate invalid login', () => {
+    before(() => {
+        Cypress.env();
+    })
+
+    it('Display error message if invalid password', () => {
+        cy.visit('/');
+        cy.get("[type='email']").type(Cypress.env('email'));
+        cy.get("[type='password']").type('Bad#passWord');
+        cy.get('[data-cy=login-with-credentials-button]').click();
+        cy.contains('Error Logging in: Error');
+    })
+
+
+})
